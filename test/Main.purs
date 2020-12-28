c0828e802bce65b9d37e359fbb454eb1c3e81fa1
John Watson <john.watson@gmx.co.uk>
2020-12-28 12:06:08 +0000

    Can we get a test to compile at least

--- test/Main.purs
+++ test/Main.purs
@@ -4,8 +4,13 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Class.Console (log)
+import OpenLayers.Style.Icon as Icon
+import OpenLayers.Style.Style as Style
 
 main :: Effect Unit
 main = do
   log "🍝"
-  log "You should add some tests."
+  log "play with the Icon."
+  picon <- Icon.create'
+  style <- Style.create { image : picon }
+  pure unit
